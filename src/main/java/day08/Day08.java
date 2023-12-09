ebc111f4c0e48c10aa69cc3c9169978c6833b5ac
ArnaudLec <lec.arnaud+github@gmail.com>
2023-12-09 23:42:34 +0100

    Day 8 - part 2

--- src/main/java/day08/Day08.java
+++ src/main/java/day08/Day08.java
@@ -1,6 +1,8 @@
 package day08;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -16,33 +18,102 @@ public class Day08 {
 	private record Directions(String left, String right) {
 	}
 
-	public static int countSteps(final String input) {
-		String[] lines = Utils.splitLines(input);
-		String route = lines[0];
+	public record WastelandMap(String route, Map<String, Directions> map) {
 
-		Map<String, Directions> map = Arrays.stream(lines)
-				.skip(2)
-				.map(DIRECTIONS_PATTERN::matcher)
-				.peek(Matcher::find)
-				.collect(Collectors.toMap(matcher -> matcher.group("place"),
-						matcher -> new Directions(matcher.group("left"), matcher.group("right"))));
+		public static WastelandMap parse(final String input) {
+			String[] lines = Utils.splitLines(input);
+			String route = lines[0];
 
-		String currentPlace = "AAA";
-		int steps = 0;
-		while (!"ZZZ".equals(currentPlace)) {
+			Map<String, Directions> map = Arrays.stream(lines)
+					.skip(2)
+					.map(DIRECTIONS_PATTERN::matcher)
+					.peek(Matcher::find)
+					.collect(Collectors.toMap(matcher -> matcher.group("place"),
+							matcher -> new Directions(matcher.group("left"), matcher.group("right"))));
+
+			return new WastelandMap(route, map);
+		}
+
+		public int countStepsPart1() {
+			String currentPlace = "AAA";
+			int steps = 0;
+			while (!"ZZZ".equals(currentPlace)) {
+				char direction = route.charAt(steps % route.length());
+				currentPlace = getNextNode(currentPlace, direction);
+				steps++;
+			}
+
+			return steps;
+		}
+
+		private String getNextNode(final String currentPlace, final char direction) {
 			Directions dirs = map.get(currentPlace);
-			char direction = route.charAt(steps % route.length());
-
-			currentPlace = switch (direction) {
+			return switch (direction) {
 			case 'L' -> dirs.left;
 			case 'R' -> dirs.right;
 			default -> throw new IllegalStateException("Direction [" + direction + "] not expected");
 			};
-
-			steps++;
 		}
 
-		return steps;
+		private char getNextDirection(final long steps) {
+			long dirIndex = steps % route.length();
+			return route.charAt(Math.toIntExact(dirIndex));
+		}
+
+		public long countStepsPart2() {
+			List<String> nodes = map.keySet().stream().filter(node -> node.endsWith("A")).toList();
+
+			int[] steps = nodes.stream().mapToInt(this::countStepsTowardsEnd).toArray();
+
+			List<List<Integer>> stepsDivisors = Arrays.stream(steps).mapToObj(Day08::getDivisors).toList();
+
+			List<Integer> commonDivisors = getCommonDivisors(stepsDivisors);
+
+			long result = stepsDivisors.stream()
+					.flatMap(List::stream)
+					.filter(Utils.not(commonDivisors::contains))
+					.mapToLong(a -> a)
+					.reduce((a, b) -> a * b)
+					.getAsLong();
+
+			return result * commonDivisors.stream().mapToInt(Integer::intValue).reduce((a, b) -> a * b).orElse(1);
+		}
+
+		private List<Integer> getCommonDivisors(final List<List<Integer>> stepsDivisors) {
+			if (stepsDivisors.size() == 1) {
+				return stepsDivisors.get(0);
+			}
+
+			List<Integer> step1Divisors = stepsDivisors.get(0);
+
+			return step1Divisors.stream()
+					.filter(divisor -> stepsDivisors.stream().allMatch(list -> list.contains(divisor)))
+					.toList();
+		}
+
+		private int countStepsTowardsEnd(final String node) {
+			String currentNode = node;
+			int steps = 0;
+			while (!currentNode.endsWith("Z")) {
+				char direction = getNextDirection(steps);
+				currentNode = getNextNode(currentNode, direction);
+				steps++;
+			}
+
+			return steps;
+		}
+
+	}
+
+	private static List<Integer> getDivisors(final int n) {
+		List<Integer> divisors = new ArrayList<>();
+		for (int i = 1; i <= n / 2; i++) {
+			if (n % i == 0) {
+				divisors.add(i);
+			}
+		}
+		divisors.add(n);
+		return divisors;
 	}
 
 }
--- src/test/java/day08/Day08Test.java
+++ src/test/java/day08/Day08Test.java
@@ -8,30 +8,29 @@ import java.nio.file.Path;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import day08.Day08.WastelandMap;
 import utils.TestUtils;
 
 public class Day08Test {
 	private static final Path INPUT_FILE_PATH = TestUtils.getInputFile("day08-input.txt");
-
-	private static final String EXAMPLE = """
-			RL
-
-			AAA = (BBB, CCC)
-			BBB = (DDD, EEE)
-			CCC = (ZZZ, GGG)
-			DDD = (DDD, DDD)
-			EEE = (EEE, EEE)
-			GGG = (GGG, GGG)
-			ZZZ = (ZZZ, ZZZ)
-			 """;
 
 	@Nested
 	class Part1 {
 
 		@Test
 		void example() throws Exception {
-			int steps = Day08.countSteps(EXAMPLE);
-			assertEquals(2, steps);
+			WastelandMap wastelandMap = WastelandMap.parse("""
+					RL
+
+					AAA = (BBB, CCC)
+					BBB = (DDD, EEE)
+					CCC = (ZZZ, GGG)
+					DDD = (DDD, DDD)
+					EEE = (EEE, EEE)
+					GGG = (GGG, GGG)
+					ZZZ = (ZZZ, ZZZ)
+					 """);
+			assertEquals(2, wastelandMap.countStepsPart1());
 		}
 
 		@Test
@@ -44,14 +43,14 @@ public class Day08Test {
 					ZZZ = (ZZZ, ZZZ)
 					""";
 
-			int steps = Day08.countSteps(example2);
-			assertEquals(6, steps);
+			WastelandMap wastelandMap = WastelandMap.parse(example2);
+			assertEquals(6, wastelandMap.countStepsPart1());
 		}
 
 		@Test
 		void inputFile() throws Exception {
-			int steps = Day08.countSteps(Files.readString(INPUT_FILE_PATH));
-			System.out.println(steps);
+			WastelandMap wastelandMap = WastelandMap.parse(Files.readString(INPUT_FILE_PATH));
+			System.out.println(wastelandMap.countStepsPart1());
 		}
 	}
 
@@ -60,13 +59,27 @@ public class Day08Test {
 
 		@Test
 		void example() throws Exception {
+			WastelandMap wastelandMap = WastelandMap.parse("""
+					LR
 
+					11A = (11B, XXX)
+					11B = (XXX, 11Z)
+					11Z = (11B, XXX)
+					22A = (22B, XXX)
+					22B = (22C, 22C)
+					22C = (22Z, 22Z)
+					22Z = (22B, 22B)
+					XXX = (XXX, XXX)
+					""");
+			assertEquals(6, wastelandMap.countStepsPart2());
 		}
 
 		@Test
 		void inputFile() throws Exception {
-
+			WastelandMap wastelandMap = WastelandMap.parse(Files.readString(INPUT_FILE_PATH));
+			System.out.println(wastelandMap.countStepsPart2());
 		}
+
 	}
 
 }
