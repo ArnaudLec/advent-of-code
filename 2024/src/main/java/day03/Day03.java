e0cb29f795b022809d6c1d102f58e80c0471cc5c
ArnaudLec <lec.arnaud+github@gmail.com>
2024-12-03 23:15:25 +0100

    2024 - Day 3 - part 2

--- 2024/src/main/java/day03/Day03.java
+++ 2024/src/main/java/day03/Day03.java
@@ -3,20 +3,30 @@ package day03;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import utils.Part;
+
 class Day03 {
 
-	private static final Pattern MULTIPLY_PATTERN = Pattern.compile("mul\\((\\d+),(\\d+)\\)");
+	private static final Pattern MULTIPLY_PATTERN = Pattern
+			.compile("mul\\((\\d{1,3}),(\\d{1,3})\\)|do\\(\\)|don't\\(\\)");
 
-	public static int calc(String input) {
+	public static int calc(String input, Part part) {
 		Matcher m = MULTIPLY_PATTERN.matcher(input);
 		int result = 0;
-
+		boolean ignore = false;
 		while (m.find()) {
-			int left = Integer.parseInt(m.group(1));
-			int right = Integer.parseInt(m.group(2));
-			result += left * right;
+			if ("do()".equals(m.group())) {
+				ignore = false;
+			} else if ("don't()".equals(m.group())) {
+				ignore = true;
+			} else if (part == Part.PART_1 || !ignore) {
+				int left = Integer.parseInt(m.group(1));
+				int right = Integer.parseInt(m.group(2));
+				result += left * right;
+			}
 		}
 
 		return result;
 	}
+
 }
--- 2024/src/test/java/day03/Day03Test.java
+++ 2024/src/test/java/day03/Day03Test.java
@@ -8,27 +8,25 @@ import java.nio.file.Path;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import utils.Part;
 import utils.TestUtils;
 
 class Day03Test {
 	private static final Path INPUT_FILE_PATH = TestUtils.getInputFile("day03-input.txt");
-
-	private static final String EXAMPLE = """
-			xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
-			""";
 
 	@Nested
 	class Part1 {
 
 		@Test
 		void example() throws Exception {
-			assertEquals(161, Day03.calc(EXAMPLE));
+			assertEquals(161,
+					Day03.calc("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))", Part.PART_1));
 		}
 
 		@Test
 		void inputFile() throws Exception {
 			String file = Files.readString(INPUT_FILE_PATH);
-			System.out.println(Day03.calc(file));
+			System.out.println(Day03.calc(file, Part.PART_1));
 		}
 	}
 
@@ -37,14 +35,14 @@ class Day03Test {
 
 		@Test
 		void example() throws Exception {
-			assertEquals(0, EXAMPLE);
+			assertEquals(48, Day03.calc("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))",
+					Part.PART_2));
 		}
 
 		@Test
 		void inputFile() throws Exception {
 			String file = Files.readString(INPUT_FILE_PATH);
-
-			System.out.println();
+			System.out.println(Day03.calc(file, Part.PART_2));
 		}
 	}
 
